6bbe1663ac3d9c7c0e31bcb7200069b80eb91de7
Richard Moore <github@ricmoo.com>
2021-07-14 14:16:14 -0400

    Initial round of projects.

---
+++ landing/projects.js
@@ -0,0 +1,74 @@
+const Projects = [{
+  image: "rkalis_allbastards-com.png",
+  name: "AllBastards",
+  url: "https://github.com/rkalis/allbastards.com",
+  score: 7
+},{
+  image: "bokkypoobah_BestBastardGANPunks.png",
+  name: "BestBastardGANPunks",
+  url: "https://github.com/bokkypoobah/BestBastardGANPunks",
+  score: 1
+},{
+  image: "compound-finance_compound-js.svg",
+  name: "Compound.js",
+  url: "https://github.com/compound-finance/compound-js",
+  score: 91
+},{
+  image: "ensdomains_ens-app.svg",
+  name: "ENS App",
+  url: "https://github.com/ensdomains/ens-app",
+  score: 79
+},{
+  image: "ethereum-optimism_optimism.svg",
+  name: "Optimism",
+  url: "https://github.com/ethereum-optimism/optimism",
+  score: 507
+/* Wait for a GitHub project?
+}, {
+  image: "holdberg.jpg",
+  name: "Holdbeg Financial",
+  url: "https://github.com/hodlberg",
+  score: 1
+},{
+  image: "",
+  name: "",
+  url: "",
+  score: 
+*/
+},{
+  image: "mycryptohq_mycrypto.png",
+  name: "MyCrpyto",
+  url: "https://github.com/MyCryptoHQ/MyCrypto",
+  score: 968
+},{
+  image: "nanexcool_daistats.png",
+  name: "Daistats",
+  url: "https://github.com/nanexcool/daistats",
+  score: 57
+},{
+  image: "rkalis_revoke-cash.png",
+  name: "Revoke",
+  url: "https://github.com/rkalis/revoke.cash",
+  score: 73
+},{
+  image: "statechannels_statechannels.svg",
+  name: "State Channels",
+  url: "https://github.com/statechannels/statechannels",
+  score: 101
+},{
+  image: "wallet3_wallet3.png",
+  name: "Wallet3",
+  url: "https://github.com/Wallet3/Wallet3",
+  score: 0
+},{
+  image: "wighawag_mandalas.png",
+  name: "Mandalas",
+  url: "https://github.com/wighawag/mandalas",
+  score: 1
+},{
+  image: "wmitsuda_otterscan.png",
+  name: "Otterscan",
+  url: "https://github.com/wmitsuda/otterscan",
+  score: 118
+}].sort((a, b) => (b.score - a.score));
+console.log(Projects);
